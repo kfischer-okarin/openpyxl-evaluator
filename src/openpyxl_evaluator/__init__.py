63a175966b404c6c5fc21fd2e3c59626c8dcc3fa
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-04-03 22:00:11 +0900

    Add empty Evaluator class

--- src/openpyxl_evaluator/__init__.py
+++ src/openpyxl_evaluator/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Kevin Fischer <kfischer_okarin@yahoo.co.jp>
 #
 # SPDX-License-Identifier: MIT
+
+from openpyxl_evaluator.evaluator import Evaluator
+
+__all__ = ["Evaluator"]
